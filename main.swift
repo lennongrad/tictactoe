e152b6b2a8ac116f16a0eecb0ef01a5ffd3dcb86
Brock Dilley <brock-dilley@codermerlin.com>
2018-09-06 09:47:35 -0500

    adding new stuff

--- main.swift
+++ main.swift
@@ -1,16 +1,26 @@
 import Foundation
 srandom(UInt32(time(nil)))
 
-var boardPos = [Int]()
 var width = 3
 var input : String
 var terminate = false
 
+enum Space : Int{
+    case empty = 0
+    case x = 1
+    case o = 2
+}
+
+var boardPos = [Space]()
+
 // fills board positions, assuming a square
 // wumbo
 for _ in 0..<(width * width){
-    boardPos.append(0)
+    boardPos.append(Space.empty)
 }
+
+var player : Space
+var ai : Space
 
 // repeats a string many times and returns as a single string on one line
 // use '\n' for spaces as it acts as a character
@@ -22,8 +32,8 @@ func mark(x: Int) -> String{
     // for the spaces in a board and other uses
     // returns X or O if applicable, or the number of the square. floats to 00X, thus allowing up to 999, ie 001, 029, or 137
     switch(boardPos[x]){
-    case 1: return " X "
-    case 2: return " O "
+    case Space.x: return " X "
+    case Space.o: return " O "
     default: if(String(x).count == 3){
                  return String(x)
              } else if(String(x).count == 2){
@@ -49,7 +59,7 @@ func board(messages: Int){
     print(repString(toRepeat: "\n", times: 20 - messages))
 }
 
-func winCheck(player: Int) -> Bool{
+func winCheck(player: Space) -> Bool{
     var win = false
 
     
@@ -87,6 +97,7 @@ func winCheck(player: Int) -> Bool{
         if(horizontal == width || leftTop == width || leftBottom == width){
             win = true
         }
+        print(leftBottom)
     }
 
     return win
@@ -94,11 +105,27 @@ func winCheck(player: Int) -> Bool{
 
 // Function used to determine which space the AI moves to each turn
 func aiMove() -> String{
+    // placeholder, only chooses random
     return String(random() % (width * width))
 }
 
 // immediately renders board
 board(messages: 1)
+
+print("Input either an X or an O (X will go first!!!)")
+input = readLine()!
+while(input != "X" && input != "O"){
+    print("Input either an X or an O (X will go first!!!)")
+    input = readLine()!
+}
+
+if(input == "X"){
+    player = Space.x
+    ai = Space.o
+} else {
+    player = Space.o
+    ai = Space.x
+}
 
 // max number of turns is equal to spaces, thus width * width. two turns per "round," ie i move for 1 turn then you move for the other 1 turn of the round
 for turn in 0..<(width * width){
@@ -106,14 +133,14 @@ for turn in 0..<(width * width){
     // move starts at -1. it is set to the value of the grid square when an int is properly submitted, thus exiting the loop
     while(move == -1 && !terminate){
         print("Player \(turn % 2 + 1)'s Turn: ", terminator:"")
-        if(turn % 2 == 0){
+        if(Space(rawValue: turn % 2 + 1) == ai){
             input = aiMove()
         } else {
             input = readLine()!
         }
         if let choice = Int(input){
             // if board space is unfilled and between the values possible
-            if(choice > -1 && choice < (width * width) && boardPos[choice] == 0){
+            if(choice > -1 && choice < (width * width) && boardPos[choice] == Space.empty){
                 move = choice
             } else {
                 board(messages: 2)
@@ -131,9 +158,13 @@ for turn in 0..<(width * width){
 
     if(!terminate){
         // turn % 2 + 1 will return 1 for player 1 and 2 for player 2 based on who should be playing each round
-        boardPos[move] = turn % 2 + 1
+        if(turn % 2 == 0){
+            boardPos[move] = Space.x
+        } else {
+            boardPos[move] = Space.o
+        }
         board(messages: 2)
-        if(winCheck(player: turn % 2 + 1)){
+        if(winCheck(player: Space(rawValue: turn % 2 + 1)!)){
             // check for wins by current player, terminate if won, and give message
             board(messages: 3)
             terminate = true
